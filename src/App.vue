34cfd5a2aeb91dcb3866504a487310d39a1434ef
Mykola Kolomoyets <mykola.kolomoyets@phenomenon-studio.com>
2024-07-26 18:40:49 +0300

    feat(data-binding): tested different data binding

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
-<script setup></script>
+<script setup>
+import MyComponent from './components/MyComponent.vue'
+</script>
 
 <template>
     <h1>Hello</h1>
+    <MyComponent />
 </template>
---
+++ src/components/MyComponent.vue
@@ -0,0 +1,14 @@
+<script setup>
+const testMessage = 'Hello!'
+const testLinkUrl = 'https://vitejs.dev/guide/features.html'
+const testImage = {
+    src: 'https://vitejs.dev/logo.svg',
+    alt: 'Vite Logo',
+}
+</script>
+
+<template>
+    <h1>testMessage: {{ testMessage }}</h1>
+    <a :href="testLinkUrl">Vite Docs</a>
+    <img v-bind="testImage" />
+</template>
