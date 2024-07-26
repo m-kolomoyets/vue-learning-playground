a0d2038cc8c7e7b85fb63806c14920c87c91a7da
Mykola Kolomoyets <mykola.kolomoyets@phenomenon-studio.com>
2024-07-26 19:23:25 +0300

    feat(ref-object-state): compared reactive and ref ovject states and rerenders

--- src/components/ReactiveSection/ReactiveSection.vue
+++ src/components/ReactiveSection/ReactiveSection.vue
@@ -1,28 +1,56 @@
 <script setup>
-import { reactive } from 'vue'
+import { reactive, ref } from 'vue'
 
-const combinedState = reactive({
+const refState = ref({
     count: 0,
     users: ['Alice', 'Bob', 'Charlie'],
 })
 
-const incrementCounter = () => {
-    combinedState.count++
+const reactiveState = reactive({
+    count: 0,
+    users: ['Alice', 'Bob', 'Charlie'],
+})
+
+const incrementReactiveCounter = () => {
+    reactiveState.count++
 }
 
-const addUser = (name) => {
-    combinedState.users.push(name)
+const incrementRefCounter = () => {
+    refState.value.count++
+}
+
+const addReactiveUser = (name) => {
+    reactiveState.users.push(name)
+}
+
+const addRefUser = (name) => {
+    refState.value.users.push(name)
 }
 </script>
 
 <!-- NOTE: Once state changes, not all object retrieves rerender, but only the changes one -->
 <template>
-    <h1>Count: {{ combinedState.count }}</h1>
-    <button @click="incrementCounter">Increment</button>
+    <br />
+    <h1>REACTIVE</h1>
+    <h3>Count: {{ reactiveState.count }}</h3>
+    <button @click="incrementReactiveCounter">Increment</button>
 
-    <h2>Users:</h2>
-    <button @click="addUser('David')">Add User</button>
+    <h3>Users:</h3>
+    <button @click="addReactiveUser('David')">Add User</button>
     <ul>
-        <li v-for="user in combinedState.users" :key="user">{{ user }}</li>
+        <li v-for="user in reactiveState.users" :key="user">{{ user }}</li>
+    </ul>
+
+    <br />
+
+    <h1>REF</h1>
+    <h3>Count: {{ refState.count }}</h3>
+    <button @click="incrementRefCounter">Increment</button>
+
+    <h3>Users:</h3>
+    <button @click="addRefUser('David')">Add User</button>
+
+    <ul>
+        <li v-for="user in refState.users" :key="user">{{ user }}</li>
     </ul>
 </template>
