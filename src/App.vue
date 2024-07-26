98167a8c0066b6f68f8434f4148c018293f8ee37
Mykola Kolomoyets <mykola.kolomoyets@phenomenon-studio.com>
2024-07-26 18:57:38 +0300

    feat(event-handlers): adde dprimitive coutner, added primitive form

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup>
 import BindingSection from './components/BindingSection.vue'
+import EventHandlersSection from './components/EventHandlersSection.vue'
 </script>
 
 <template>
     <h1>Hello</h1>
     <BindingSection />
+    <EventHandlersSection />
 </template>
---
+++ src/components/EventHandlersSection.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { ref } from 'vue'
+const count = ref(0)
+
+const increment = () => {
+    count.value++
+}
+
+const decrement = () => {
+    count.value--
+}
+
+const formSubmitHandler = (event) => {
+    event.preventDefault()
+
+    const firstName = event.target[0].value
+    const lastName = event.target[1].value
+
+    console.log('Form submitted!', { firstName, lastName })
+}
+</script>
+
+<template>
+    <h1>Count: {{ count }}</h1>
+    <button @click="increment">Increment</button>
+    <button @click="decrement">Decrement</button>
+
+    <form @submit="formSubmitHandler">
+        <input type="text" placeholder="First Name" />
+        <input type="text" placeholder="Last Name" />
+        <button type="submit">Submit</button>
+    </form>
+</template>
